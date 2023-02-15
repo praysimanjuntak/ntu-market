6483132bcff22ea7da1699fb686e77eebfd7d0b5
jemctoh <85207590+jemctoh@users.noreply.github.com>
2023-02-16 01:48:52 +0800

    Revert "Bhar"

--- MERN base/client/src/App.jsx
+++ MERN base/client/src/App.jsx
@@ -1,28 +1,22 @@
 
 //functions
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react';
 import { getTest } from "./api/test";
 import Login from "./pages/Login";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Signup from "./pages/Signup";
-import Navbar from './components/navbar';
 
 function App() {
   
 
   return (
-    <ChakraProvider>
-      <div>
-        <Navbar/>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<Login/>}/>
-            <Route path='/sign-up' element={<Signup/>}/>
-          </Routes>
-        </Router>
-      </div>
-    </ChakraProvider>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path='/sign-up' element={<Signup/>}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
