edfd1577cf6ff4f572012d9828fd099640b7cf78
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-02-25 01:27:04 +0800

    Update ModalIntro.js

--- MERN base/client/src/pages/ModalIntro.js
+++ MERN base/client/src/pages/ModalIntro.js
@@ -1,15 +1,15 @@
-import { Avatar, Button, Img } from "@chakra-ui/react";
+import { Avatar, Stack, Img } from "@chakra-ui/react";
 import Background from "../components/Background";
 import Footer from "../components/Footer";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import MyButton from "../components/MyButton";
 
 function ModalIntro() {
   const [showButton, setShowButton] = useState(false);
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowButton(true);
-    }, 5000);
+    }, 3000);
     return () => clearTimeout(timer);
   },[]);
 
@@ -20,13 +20,14 @@ function ModalIntro() {
         alignItems="center"
         isDarkBackground
         backgroundColor="#313690"
+        spacing={5}
         >
         <Img src ='/LOGO.png'></Img>
-        <div>
+        <Stack textColor='black'>
         {showButton && (
-        <Link to={'/house'} colorScheme='white'>Enter</Link>
+        MyButton('/house', 'Enter')
         )}
-        </div>
+        </Stack>
         
     </Background>
     
